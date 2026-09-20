995597ee98d1862a84b8e1b08c7d311340e58a53
akshayhase904-hub <akshay.hase.904@gmail.com>
2026-09-20 19:05:22 +0530

    Delete backup directory

--- backup/fix_indent2.py
+++
@@ -1,7 +0,0 @@
-with open("main.py", "r", encoding="utf-8") as f:
-    content = f.read()
-
-content = content.replace("        import trade_manager\n", "            import trade_manager\n")
-
-with open("main.py", "w", encoding="utf-8") as f:
-    f.write(content)
